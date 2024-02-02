4f804dfa8690921b7a1aa9cb12747412f3a64dc0
tristan <thuckabee.work@gmail.com>
2024-02-02 16:14:31 -0600

    set up a server to host project

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.end('hello?')
+  if (req.method === 'GET' && req.url.endsWith('.css')) {
+    const fileName = req.url.split('/')[1]
+    const css = fs.readFileSync(`./assets/${fileName}`, 'utf-8');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/css');
+    return res.end(css)
+  }
+  if (req.method === 'GET' && req.url.endsWith('.js')) {
+    const fileName = req.url.split('/')[1]
+    const jsFile = fs.readFileSync(`./assets/${fileName}`, 'utf-8');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/javascript');
+    return res.end(jsFile)
+  }
+  if (req.method === 'GET' && req.url === '/') {
+    const html = fs.readFileSync('./assets/index.html', 'utf-8');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    return res.end(html)
+  }
 });
 
 const port = 5500;
-server.listen(port, () => {});
+server.listen(port, () => console.log('Live on p:5500'));
